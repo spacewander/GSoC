4afbb82c4ccad3cdc944cd7a6a6b64916741efc2
spacewander <spacewanderlzx@gmail.com>
2014-10-17 22:45:00 +0800

    print notice if not result found

--- index.js
+++ index.js
@@ -65,7 +65,12 @@ if (argv.t && !isArray(argv.t)) {
   argv.t = new Array(argv.t);
 }
 
+// print the result with given projects
 var printProjects = function(projects) {
+  // if projects is empty
+  if (!Object.keys(projects).length) {
+    return console.log('Not result found.');
+  }
   for (var key in projects) {
     console.log(key + "\t\t\t\t: " + projects[key]);
   }
