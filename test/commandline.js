cb23e537d1a82585cad73bf03dfe0b89aa1806d1
spacewander <spacewanderlzx@gmail.com>
2014-10-19 11:30:21 +0800

    mostly completed

--- test/commandline.js
+++ test/commandline.js
@@ -15,45 +15,45 @@ describe('commandline options: ', function(){
 
   describe('List projects ', function(){
     it('GSoC', function(){
-      exec(script, {silent: true}).output.should.equal('Rails\t\t\t\t: ruby\nSciRuby\t\t\t\t: ruby\n');
+      exec(script, {silent: true}).output.should.equal('\u001b[33mRails\u001b[39m\u001b[36m\n\t: ruby\u001b[39m\n\u001b[33mSciRuby\u001b[39m\u001b[36m\n\t: ruby\u001b[39m\n');
     });
 
     it('GSoC 2013', function(){
-      exec(script + ' 2013', {silent: true}).output.should.equal('AbiWord\t\t\t\t: wordProcess\nRails\t\t\t\t: ruby\n');
+      exec(script + ' 2013', {silent: true}).output.should.equal('\u001b[33mAbiWord\u001b[39m\u001b[36m\n\t: wordProcess\u001b[39m\n\u001b[33mRails\u001b[39m\u001b[36m\n\t: ruby\u001b[39m\n');
     });
 
     it('GSoC 2013-2014', function(){
-      exec(script + ' 2013-2014', {silent: true}).output.should.equal('Rails\t\t\t\t: ruby\n');
+      exec(script + ' 2013-2014', {silent: true}).output.should.equal('\u001b[33mRails\u001b[39m\u001b[36m\n\t: ruby\u001b[39m\n');
     });
   });
 
   describe('list with tags ', function(){
     it('GSoC -t ruby', function(){
-      exec(script + ' -t ruby', {silent: true}).output.should.equal('Rails\t\t\t\t: ruby\nSciRuby\t\t\t\t: ruby\n');
+      exec(script + ' -t ruby', {silent: true}).output.should.equal('\u001b[33mRails\u001b[39m\u001b[36m\n\t: ruby\u001b[39m\n\u001b[33mSciRuby\u001b[39m\u001b[36m\n\t: ruby\u001b[39m\n');
     });
 
     it('GSoC -t ruby -t web', function(){
-      exec(script + ' -t ruby -t web', {silent: true}).output.should.equal('Not result found.\n');
+      exec(script + ' -t ruby -t web', {silent: true}).output.should.equal('\u001b[31mNot result found.\u001b[39m\n');
     });
   });
 
   describe('list with name ', function(){
     it('GSoC -n R', function(){
-      exec(script + ' -n R', {silent: true}).output.should.equal('Rails\t\t\t\t: ruby\nSciRuby\t\t\t\t: ruby\n');
+      exec(script + ' -n R', {silent: true}).output.should.equal('\u001b[33mRails\u001b[39m\u001b[36m\n\t: ruby\u001b[39m\n\u001b[33mSciRuby\u001b[39m\u001b[36m\n\t: ruby\u001b[39m\n');
     });
 
     it('GSoC 2009-2012 -n a', function(){
-      exec(script + ' 2009-2012 -n a', {silent: true}).output.should.equal('AbiWord\t\t\t\t: wordProcess\n');
+      exec(script + ' 2009-2012 -n a', {silent: true}).output.should.equal('\u001b[33mAbiWord\u001b[39m\u001b[36m\n\t: wordProcess\u001b[39m\n');
     });
   });
 
   describe('list with both tags and name', function(){
     it('GSoC 2014 -n c -t ruby', function(){
-      exec(script + ' -n c -t ruby', {silent: true}).output.should.equal('SciRuby\t\t\t\t: ruby\n');
+      exec(script + ' -n c -t ruby', {silent: true}).output.should.equal('\u001b[33mSciRuby\u001b[39m\u001b[36m\n\t: ruby\u001b[39m\n');
     });
 
     it('GSoC 2010 -n de -t c -t java', function(){
-      exec(script + ' 2010 -n de -t c -t java', {silent: true}).output.should.equal('Debian Project\t\t\t\t: c,java,python\n');
+      exec(script + ' 2010 -n de -t c -t java', {silent: true}).output.should.equal('\u001b[33mDebian Project\u001b[39m\u001b[36m\n\t: c,java,python\u001b[39m\n');
     });
   });
 
